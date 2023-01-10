d75c79c1b74f99d4073515d28d18c7772c966579
Armin Abbasi <apmnh.armin@gmail.com>
2023-01-10 14:41:32 +0100

    Successful Test

--- pyter.py
+++ pyter.py
@@ -6,6 +6,7 @@ import requests
 
 
 url = "https://files.realpython.com/media/How-to-Run-A-Python-Script_Watermarked.65fe32bf5487.jpg"
+pieces = []
 
 
 def file_size(url: str) -> int:
@@ -24,20 +25,19 @@ def chop(url, threads):
     return chunks
 
 
-async def download_file(url: str, start: int, end: int, target) -> None:
-    async with aiohttp.ClientSession(headers={"Range": f"bytes={start}-{end}"}, raise_for_status=True) as session:
-        async with session.get(url) as response:
-            async with aiofiles.open(target, mode='xb') as file:
-                await file.write(await response.read())
+async def download_file(url: str, start: int, end: int) -> None:
+    async with aiohttp.ClientSession(raise_for_status=True) as session:
+        async with session.get(url, headers={"Range": f"bytes={start}-{end}"}) as response:
+            await pieces.append((start, await response.read()))
 
 
-async def manager(url: str, target: str, threads: int = 16):
+async def manager(url: str, threads: int = 16):
     chunks = chop(url, threads)
     tasks = []
 
     for j in range(len(chunks) - 1):
         task = asyncio.ensure_future(download_file(
-            url, chunks[j], chunks[j + 1] - 1, target))
+            url, chunks[j], chunks[j + 1] - 1))
 
         tasks.append(task)
 
@@ -45,7 +45,7 @@ async def manager(url: str, target: str, threads: int = 16):
 
 
 # threads = 16
-size = int(requests.head(url).headers["content-length"])
+# size = int(requests.head(url).headers["content-length"])
 # chunk = size // threads
 # chunks = []
 
@@ -65,11 +65,6 @@ size = int(requests.head(url).headers["content-length"])
 #         for chunk in piece.iter_content(chunk_size=128):
 #             file.write(chunk)
 
-
-asyncio.run(manager(
-    url, "/home/armin/Downloads/f/How-to-Run-A-Python-Script_Watermarked.65fe32bf5487.jpg"))
-
-
 # async def test():
 #     async with aiohttp.ClientSession() as session:
 #         async with session.get(url) as resp:
@@ -80,3 +75,12 @@ asyncio.run(manager(
 
 
 # asyncio.run(test())
+
+asyncio.run(manager(
+    url, "/home/armin/Downloads/f/How-to-Run-A-Python-Script_Watermarked.65fe32bf5487.jpg"))
+
+pieces.sort(key=lambda item: item[0])
+
+with open("/home/armin/Downloads/f/How-to-Run-A-Python-Script_Watermarked.65fe32bf5487.jpg", "wb") as file:
+    for piece in pieces:
+        file.write(piece[1])
