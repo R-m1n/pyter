df7228d1f5b07bb046fb95224f85057f24d4c502
Armin Abbasi <apmnh.armin@gmail.com>
2023-01-13 21:33:13 +0100

    Add feature

--- pyter.py
+++ pyter.py
@@ -2,23 +2,22 @@ import requests
 import asyncio
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
-from urllib.parse import urlparse
 from requests import Session
 from aiohttp import ClientSession
 from queue import PriorityQueue
 from typing import Coroutine
-from tqdm.asyncio import trange
+from tqdm import trange
+from tqdm.asyncio import tqdm_asyncio
 
 
 async def download_file(url: str, start: int, end: int) -> Coroutine:
     async with ClientSession(raise_for_status=True) as session:
         async with session.get(url, headers={"Range": f"bytes={start}-{end}"}) as response:
-            async for chunk_size in trange((end - start) // 1024, colour="#77C3EC", unit="kB"):
-                pieces.put((start, await response.read()))
+            pieces.put((start, await response.read()))
 
 
 async def manager(url: str, chunks: list[int]) -> Coroutine:
-    tasks = []
+    tasks = [asyncio.ensure_future(asyncio.sleep(1))]
 
     for j in range(len(chunks) - 1):
         task = asyncio.ensure_future(
@@ -27,11 +26,18 @@ async def manager(url: str, chunks: list[int]) -> Coroutine:
 
         tasks.append(task)
 
-    await asyncio.gather(*tasks)
+    await tqdm_asyncio.gather(*tasks, timeout=100, colour="#77C3EC", unit="kB")
 
 
 def _file_size(url: str) -> int:
-    return int(Session().head(url).headers["content-length"])
+    response = Session().head(url)
+
+    _test_connection(response)
+
+    if "content-length" in response.headers:
+        return int(response.headers["content-length"])
+
+    return 0
 
 
 def _chop(size: int, threads: int) -> list[int]:
@@ -64,6 +70,11 @@ def _getArgs() -> Namespace:
     return parser.parse_args()
 
 
+def _test_connection(response: requests.Response) -> None:
+    if response.status_code == requests.codes.ok:
+        raise requests.exceptions.ConnectionError()
+
+
 if __name__ == "__main__":
     DEFAULT_THREADS = 16
 
@@ -80,17 +91,20 @@ if __name__ == "__main__":
         tdir.mkdir()
 
     if size != 0:
-        threads = args.threads if args.threads else DEFAULT_THREADS
+        threads = args.threads - 1 if args.threads else DEFAULT_THREADS - 1
 
         chunks = _chop(size, threads)
 
         pieces = PriorityQueue()
 
+        print("\n")
         asyncio.run(manager(url, chunks))
 
-        with open(tdir / Path(url).name, "wb") as file:
-            while not pieces.empty():
-                file.write(pieces.get()[1])
+        print("\n")
+        for chunk_size in trange(size // 1024, colour="#77C3EC", unit="kB"):
+            with open(tdir / Path(url).name, "wb") as file:
+                while not pieces.empty():
+                    file.write(pieces.get()[1])
 
     else:
         result = requests.get(url)
